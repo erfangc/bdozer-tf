9e3eb3b5aea2a7a6a31def8093a9777c4ec46f40
Erfang Chen <erfangc@gmail.com>
2021-07-08 17:53:45 -0400

    wip

--- main.tf
+++ main.tf
@@ -25,6 +25,23 @@ module "vpc" {
 
   tags = {
     Owner       = "user"
-    Environment = "me"
+    Environment = "Development"
+  }
+}
+
+module "ecs" {
+  source             = "terraform-aws-modules/ecs/aws"
+  name               = "my-ecs"
+  container_insights = true
+  capacity_providers = ["FARGATE", "FARGATE_SPOT"]
+
+  default_capacity_provider_strategy = [
+    {
+      capacity_provider = "FARGATE_SPOT"
+    }
+  ]
+
+  tags = {
+    Environment = "Development"
   }
 }
