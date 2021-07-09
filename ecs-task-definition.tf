8b1e6016efdc53be62b75e03a72f4c92311ff8c3
Erfang Chen <erfangc@gmail.com>
2021-07-09 19:51:25 -0400

    wip

--- ecs-task-definition.tf
+++ ecs-task-definition.tf
@@ -1,7 +1,7 @@
-# resource "aws_cloudwatch_log_group" "web-server" {
-#   name              = "web-server"
-#   retention_in_days = 1
-# }
+resource "aws_cloudwatch_log_group" "web-server" {
+  name              = "web-server"
+  retention_in_days = 1
+}
 
 # resource "aws_ecs_task_definition" "web-server" {
 #   execution_role_arn = "arn:aws:iam::299541157397:role/ecsTaskExecutionRole"
