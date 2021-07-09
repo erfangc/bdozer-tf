5ade121015c0a99c174ba18f2c2baa5d15bac76c
Erfang Chen <erfangc@gmail.com>
2021-07-08 21:48:10 -0400

    wip

---
+++ lb.tf
@@ -0,0 +1,26 @@
+resource "aws_lb" "lb" {
+  name               = "web-server-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [security_groups.alb-sg.id]
+  subnets            = module.vpc.public_subnets
+
+}
+
+resource "aws_lb_target_group" "lb-tg" {
+  name        = "web-server-lb-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = module.vpc.vpc_id
+}
+
+resource "aws_lb_listener" "lb-listener" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.lb-tg.arn
+  }
+}
