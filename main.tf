fd6df5c76e87dbb17939fa866187c6f23b3ff381
Erfang Chen <erfangc@gmail.com>
2021-07-08 21:14:16 -0400

    wip

--- main.tf
+++ main.tf
@@ -46,6 +46,7 @@ resource "aws_cloudwatch_log_group" "web-server" {
 }
 
 resource "aws_ecs_task_definition" "web-server" {
+  execution_role_arn       = "arn:aws:iam::299541157397:role/ecsTaskExecutionRole"
   family                   = "web-server"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
