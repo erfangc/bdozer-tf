8997a384bcd1860fd4ed6e28c114c8cfe1abfe81
Erfang Chen <erfangc@gmail.com>
2021-07-10 09:30:33 -0400

    wip

--- ecs-service/main.tf
+++ ecs-service/main.tf
@@ -14,7 +14,7 @@ resource "aws_ecr_repository" "ecr_repo" {
 #
 resource "aws_lb_target_group" "lb-tg" {
   name        = var.service_name
-  port        = 80
+  port        = var.container_port
   protocol    = "HTTP"
   target_type = "ip"
   vpc_id      = var.vpc_id
