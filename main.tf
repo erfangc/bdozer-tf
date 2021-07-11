30ed7a05ba6289b0eeecf3519c04c5c0c3ba3392
Erfang Chen <erfangc@gmail.com>
2021-07-11 09:17:06 -0400

    wip

--- main.tf
+++ main.tf
@@ -2,8 +2,9 @@
 # the ECS services we will be running
 #
 module "stock-valuation-service" {
-  source       = "./ecs-service"
-  service_name = "stock-valuation-service"
+  source        = "./ecs-service"
+  service_name  = "stock-valuation-service"
+  desired_count = 1
 
   vpc                 = module.vpc
   aws_lb_listener_arn = aws_lb_listener.lb-listener.arn
