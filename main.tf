264dc29f3fcf42a69ab0d9a4374924c093848f0e
Erfang Chen <erfangc@gmail.com>
2021-07-10 21:17:17 -0400

    wip

--- main.tf
+++ main.tf
@@ -19,3 +19,14 @@ module "web-server-b" {
   vpc                 = module.vpc
   aws_lb_listener_arn = aws_lb_listener.lb-listener.arn
 }
+
+#
+# the ECS services we will be running
+#
+module "stock-valuation-service" {
+  source       = "./ecs-service"
+  service_name = "stock-valuation-service"
+
+  vpc                 = module.vpc
+  aws_lb_listener_arn = aws_lb_listener.lb-listener.arn
+}
