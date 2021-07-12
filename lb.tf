f9eed0adad985ec19c5e98206263f1697c16e478
Erfang Chen <erfangc@gmail.com>
2021-07-11 21:35:21 -0400

    wip

--- lb.tf
+++ lb.tf
@@ -23,3 +23,32 @@ resource "aws_lb_listener" "lb-listener" {
 output "lb_dns_name" {
   value = aws_lb.lb.dns_name
 }
+
+#################
+# test out NLBs #
+#################
+resource "aws_lb_target_group" "nlb-tg" {
+  name     = "nlb-tg"
+  port     = 80
+  protocol = "TCP"
+  vpc_id   = module.vpc.vpc_id
+}
+
+resource "aws_lb" "nlb" {
+  name               = "test-nlb"
+  internal           = false
+  load_balancer_type = "network"
+  security_groups    = [aws_security_group.alb-sg.id]
+  subnets            = module.vpc.public_subnets
+}
+
+resource "aws_lb_listener" "nlb-listener" {
+  load_balancer_arn = aws_lb.front_end.arn
+  port              = "80"
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nlb-tg.arn
+  }
+}
