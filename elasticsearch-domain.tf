b42b32d2f1baca293ebd0a2337649df0c1d605e5
Erfang Chen <erfangc@gmail.com>
2021-08-01 08:15:03 -0400

    added elasticsearch-domain.tf

---
+++ elasticsearch-domain.tf
@@ -0,0 +1,28 @@
+locals {
+  domain: "elasticsearch-domain"
+}
+resource "aws_elasticsearch_domain" "esd" {
+  domain_name = local.domain
+  elasticsearch_version = "7.10"
+
+  cluster_config {
+    instance_type = "t2.small.elasticsearch"
+  }
+
+  vpc_options {
+    subnet_ids = module.vpc.private_subnets
+    security_group_ids = [aws_security_group.elasticsearch-domain-sg.id]
+  }
+
+  access_policies = jsonencode({
+    Version = "2012-10-17",
+    Statement: [
+      {
+        Action = "es:*",
+        Principal = "*",
+        Effect = "Allow",
+        Resource = "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/${local.domain}/*"
+      }
+    ]
+  })
+}
--- security-groups.tf
+++ security-groups.tf
@@ -1,48 +1,74 @@
+resource "aws_security_group" "elasticsearch-domain-sg" {
+  name = "Elasticsearch domain security group"
+  description = ""
+  vpc_id = module.vpc.vpc_id
+}
+
+resource "aws_security_group_rule" "elasticsearch-allow-https" {
+  from_port = 443
+  to_port = 443
+  protocol = "tcp"
+  security_group_id = aws_security_group.elasticsearch-domain-sg.id
+  cidr_blocks = [
+    module.vpc.vpc_cidr_block
+  ]
+  type = "ingress"
+}
+
 #
 # the ALB's security group - which allow port 80 ingress only (and obviously should be able to talk to the VPC that it is fronting)
 #
 resource "aws_security_group" "alb-sg" {
 
-  name        = "alb-sg"
+  name = "alb-sg"
   description = "Security group for the Application load balancer"
-  vpc_id      = module.vpc.vpc_id
+  vpc_id = module.vpc.vpc_id
 
   ingress = [
     {
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      security_groups  = []
-      prefix_list_ids  = []
-      self             = false,
-      description      = "Ingress from internet"
-      from_port        = 80
-      protocol         = "tcp"
-      to_port          = 80
+      cidr_blocks = [
+        "0.0.0.0/0"
+      ]
+      ipv6_cidr_blocks = [
+        "::/0"
+      ]
+      security_groups = []
+      prefix_list_ids = []
+      self = false,
+      description = "Ingress from internet"
+      from_port = 80
+      protocol = "tcp"
+      to_port = 80
     },
     {
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      security_groups  = []
-      prefix_list_ids  = []
-      self             = false,
-      description      = "Ingress from internet vai SSL"
-      from_port        = 443
-      protocol         = "tcp"
-      to_port          = 443
+      cidr_blocks = [
+        "0.0.0.0/0"]
+      ipv6_cidr_blocks = [
+        "::/0"]
+      security_groups = []
+      prefix_list_ids = []
+      self = false,
+      description = "Ingress from internet vai SSL"
+      from_port = 443
+      protocol = "tcp"
+      to_port = 443
     }
   ]
 
-  egress = [{
-    cidr_blocks      = [module.vpc.vpc_cidr_block]
-    ipv6_cidr_blocks = ["::/0"]
-    security_groups  = []
-    prefix_list_ids  = []
-    self             = false,
-    description      = "Egress to VPC"
-    from_port        = 0
-    protocol         = "tcp"
-    to_port          = 65535
-  }]
+  egress = [
+    {
+      cidr_blocks = [
+        module.vpc.vpc_cidr_block]
+      ipv6_cidr_blocks = [
+        "::/0"]
+      security_groups = []
+      prefix_list_ids = []
+      self = false,
+      description = "Egress to VPC"
+      from_port = 0
+      protocol = "tcp"
+      to_port = 65535
+    }]
 
   tags = {
     "Name" = "alb-sg"
