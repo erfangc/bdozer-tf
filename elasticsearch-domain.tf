56ae751dd2cde4ac3bb17383c47e13185dec5d22
Erfang Chen <erfangc@gmail.com>
2021-08-02 21:26:42 -0400

    wip

--- elasticsearch-domain.tf
+++ elasticsearch-domain.tf
@@ -6,38 +6,38 @@ resource "aws_iam_service_linked_role" "es" {
   aws_service_name = "es.amazonaws.com"
 }
 
-//resource "aws_elasticsearch_domain" "esd" {
-//  domain_name = local.domain
-//  elasticsearch_version = "7.10"
-//
-//  cluster_config {
-//    instance_type = "t3.small.elasticsearch"
-//    instance_count = 1
-//  }
-//
-//  vpc_options {
-//    subnet_ids = [
-//      module.vpc.private_subnets[0]
-//    ]
-//    security_group_ids = [
-//      aws_security_group.elasticsearch-domain-sg.id
-//    ]
-//  }
-//
-//  ebs_options {
-//    ebs_enabled = true
-//    volume_size = 10
-//  }
-//
-//  access_policies = jsonencode({
-//    Version = "2012-10-17",
-//    Statement: [
-//      {
-//        Action = "es:*",
-//        Principal = "*",
-//        Effect = "Allow",
-//        Resource = "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/${local.domain}/*"
-//      }
-//    ]
-//  })
-//}
+resource "aws_elasticsearch_domain" "esd" {
+  domain_name = local.domain
+  elasticsearch_version = "7.10"
+
+  cluster_config {
+    instance_type = "t3.small.elasticsearch"
+    instance_count = 3
+  }
+
+  vpc_options {
+    subnet_ids = [
+      module.vpc.private_subnets
+    ]
+    security_group_ids = [
+      aws_security_group.elasticsearch-domain-sg.id
+    ]
+  }
+
+  ebs_options {
+    ebs_enabled = true
+    volume_size = 10
+  }
+
+  access_policies = jsonencode({
+    Version = "2012-10-17",
+    Statement: [
+      {
+        Action = "es:*",
+        Principal = "*",
+        Effect = "Allow",
+        Resource = "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/${local.domain}/*"
+      }
+    ]
+  })
+}
