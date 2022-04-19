d45eca11ac3f8099fc9df70ac805550644fde595
Erfang Chen <erfangc@gmail.com>
2022-04-19 19:41:45 -0400

    wip

--- main-server.tf
+++ main-server.tf
@@ -27,6 +27,7 @@ resource "aws_security_group_rule" "main-server-ssh-ingress" {
   to_port           = 22
   protocol          = "tcp"
   security_group_id = aws_security_group.main-server.id
+  cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group_rule" "main-server-elasticsearch-ingress" {
