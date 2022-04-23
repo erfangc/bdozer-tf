14e2a1536bba21938ce9cef82abcbb46880f00dd
Erfang Chen <erfangc@gmail.com>
2022-04-22 20:46:16 -0400

    getting rid of subnets and NATs

--- vpc.tf
+++ vpc.tf
@@ -5,9 +5,7 @@ module "vpc" {
   name               = var.env
   cidr               = "10.10.10.0/24"
   azs                = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets    = ["10.10.10.0/27", "10.10.10.32/27", "10.10.10.64/27"]
   public_subnets     = ["10.10.10.96/27", "10.10.10.128/27", "10.10.10.160/27"]
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_nat_gateway = false
 }
 
