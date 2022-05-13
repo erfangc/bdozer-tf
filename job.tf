24067bc4653795b91afc0e01e072ccfb023e21ec
Erfang Chen <erfangc@gmail.com>
2022-05-12 20:11:41 -0400

    wip

--- job.tf
+++ job.tf
@@ -2,14 +2,20 @@ resource "aws_ecs_task_definition" "ubuntu" {
   family                   = "ubuntu"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
+  cpu                      = 10
+  memory                   = 512
   container_definitions    = jsonencode([
     {
       name         = "default"
       image        = "ubuntu"
-      cpu          = 10
-      memory       = 512
       essential    = true
       cmd : ["echo", "-e", "Hello World"]
+#      portMappings = [
+#        {
+#          containerPort = 80
+#          hostPort      = 80
+#        }
+#      ]
     },
   ])
 }
