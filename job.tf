31eeec610e8019893a8681b4f417149c21307c58
Erfang Chen <erfangc@gmail.com>
2022-05-12 21:19:48 -0400

    wip

--- job.tf
+++ job.tf
@@ -23,3 +23,66 @@ resource "aws_ecs_task_definition" "ubuntu" {
     },
   ])
 }
+
+//
+// schedule the run
+//
+resource "aws_iam_role" "ecs_events" {
+  name = "ecs_events"
+
+  assume_role_policy = <<DOC
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "events.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+DOC
+}
+
+resource "aws_iam_role_policy" "ecs_events_run_task_with_any_role" {
+  name = "ecs_events_run_task_with_any_role"
+  role = aws_iam_role.ecs_events.id
+
+  policy = <<DOC
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "iam:PassRole",
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": "ecs:RunTask",
+            "Resource": "*"
+        }
+    ]
+}
+DOC
+}
+
+resource "aws_cloudwatch_event_rule" "every_5_minutes" {
+  name        = "every_5_minutes"
+  schedule_expression = "rate(5 minutes)"
+}
+
+resource "aws_cloudwatch_event_target" "ecs_scheduled_task" {
+  target_id = "run-scheduled-task-every-hour"
+  arn       = aws_ecs_cluster.ecs-cluster.arn
+  rule      = aws_cloudwatch_event_rule.every_5_minutes.name
+  role_arn  = aws_iam_role.ecs_events.arn
+
+  ecs_target {
+    task_count          = 1
+    task_definition_arn = aws_ecs_task_definition.ubuntu.arn
+  }
+}
