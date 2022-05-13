f53404895fd7ca0b92de4e19eb0124c24a03c2ce
Erfang Chen <erfangc@gmail.com>
2022-05-12 23:48:55 -0400

    wip

--- job.tf
+++ job.tf
@@ -30,44 +30,40 @@ resource "aws_ecs_task_definition" "ubuntu" {
 resource "aws_iam_role" "ecs_events" {
   name = "ecs_events"
 
-  assume_role_policy = <<DOC
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "events.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-DOC
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "",
+        Effect    = "Allow",
+        Principal = {
+          Service = "events.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
 }
 
 resource "aws_iam_role_policy" "ecs_events_run_task_with_any_role" {
   name = "ecs_events_run_task_with_any_role"
   role = aws_iam_role.ecs_events.id
 
-  policy = <<DOC
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": "iam:PassRole",
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": "ecs:RunTask",
-            "Resource": "*"
-        }
+  policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "iam:PassRole",
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow",
+        Action   = "ecs:RunTask",
+        Resource = "*"
+      }
     ]
-}
-DOC
+  })
 }
 
 resource "aws_cloudwatch_event_rule" "every_5_minutes" {
