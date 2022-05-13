a3b0390a66c2fe4abb8fea39e8c5659a1e3e29ae
Erfang Chen <erfangc@gmail.com>
2022-05-12 20:39:24 -0400

    wip

--- ecs-cluster.tf
+++ ecs-cluster.tf
@@ -7,11 +7,8 @@ resource "aws_ecs_cluster" "ecs-cluster" {
   capacity_providers = ["FARGATE"]
   configuration {
     execute_command_configuration {
-      kms_key_id = aws_kms_key.master-kms-key.arn
       logging    = "OVERRIDE"
-
       log_configuration {
-        cloud_watch_encryption_enabled = true
         cloud_watch_log_group_name     = aws_cloudwatch_log_group.ecs-cluster.name
       }
     }
