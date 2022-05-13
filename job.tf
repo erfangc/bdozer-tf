9b4c4466594256fb3f938778a2e8806519c2f8e0
Erfang Chen <erfangc@gmail.com>
2022-05-12 20:26:08 -0400

    wip

--- job.tf
+++ job.tf
@@ -1,5 +1,19 @@
 resource "aws_iam_role" "ecsTaskExecutionRole" {
-  assume_role_policy  = ""
+  assume_role_policy  = <<EOF
+  {
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
   managed_policy_arns = ['arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy']
 }
 
