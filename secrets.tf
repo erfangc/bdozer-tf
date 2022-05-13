44c2571ad4a6f4973ab24cfdef6ac274a93cfb84
Erfang Chen <erfangc@gmail.com>
2022-05-13 08:16:04 -0400

    adding aws secrets

---
+++ secrets.tf
@@ -0,0 +1,28 @@
+resource "aws_secretsmanager_secret" "elasticsearch_credential" {
+  name        = "elasticsearch_credential"
+  description = "elasticsearch_credential"
+}
+resource "aws_secretsmanager_secret" "elasticsearch_endpoint" {
+  name        = "elasticsearch_endpoint"
+  description = "elasticsearch_endpoint"
+}
+resource "aws_secretsmanager_secret" "jdbc_password" {
+  name        = "jdbc_password"
+  description = "jdbc_password"
+}
+resource "aws_secretsmanager_secret" "jdbc_url" {
+  name        = "jdbc_url"
+  description = "jdbc_url"
+}
+resource "aws_secretsmanager_secret" "jdbc_username" {
+  name        = "jdbc_username"
+  description = "jdbc_username"
+}
+resource "aws_secretsmanager_secret" "polygon_api_key" {
+  name        = "polygon_api_key"
+  description = "polygon_api_key"
+}
+resource "aws_secretsmanager_secret" "quandl_api_key" {
+  name        = "quandl_api_key"
+  description = "quandl_api_key"
+}
