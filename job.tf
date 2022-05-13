23537d4de884ea2f22ec49829372fa18f4b9010e
Erfang Chen <erfangc@gmail.com>
2022-05-12 20:30:46 -0400

    wip

--- job.tf
+++ job.tf
@@ -21,7 +21,7 @@ resource "aws_ecs_task_definition" "ubuntu" {
   network_mode             = "awsvpc"
   cpu                      = 256
   memory                   = 512
-  execution_role_arn       = aws_iam_role.main-server-role.arn
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
   container_definitions    = jsonencode([
     {
       name             = "default"
