f6640a63629c2e8830384274f208bc94ce1cc2ff
Erfang Chen <erfangc@gmail.com>
2022-05-12 20:09:12 -0400

    adding a job

--- job.tf
+++ job.tf
@@ -1,13 +1,15 @@
 resource "aws_ecs_task_definition" "ubuntu" {
-  family = "ubuntu"
-  container_definitions = jsonencode([
+  family                   = "ubuntu"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  container_definitions    = jsonencode([
     {
-      name      = "default"
-      image     = "ubuntu"
-      cpu       = 10
-      memory    = 512
-      essential = true
-      cmd: ["echo", "-e", "Hello World"]
+      name         = "default"
+      image        = "ubuntu"
+      cpu          = 10
+      memory       = 512
+      essential    = true
+      cmd : ["echo", "-e", "Hello World"]
       portMappings = [
         {
         }
