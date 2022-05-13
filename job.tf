bf6c22c8b8ca45281c77d889c5bb2181e7d8b933
Erfang Chen <erfangc@gmail.com>
2022-05-12 20:25:10 -0400

    wip

--- job.tf
+++ job.tf
@@ -1,30 +1,36 @@
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  assume_role_policy  = ""
+  managed_policy_arns = ['arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy']
+}
+
 resource "aws_ecs_task_definition" "ubuntu" {
   family                   = "ubuntu"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = 256
   memory                   = 512
+  execution_role_arn       = aws_iam_role.main-server-role.arn
   container_definitions    = jsonencode([
     {
-      name         = "default"
-      image        = "ubuntu"
-      essential    = true
-      cmd = ["echo", "-e", "Hello World"]
+      name             = "default"
+      image            = "ubuntu"
+      essential        = true
+      cmd              = ["echo", "-e", "Hello World"]
       logConfiguration = {
         logDriver = "awslogs",
-        options = {
-          awslogs-group = "ubuntu-container",
-          awslogs-region = "us-east-1",
-          awslogs-create-group = "true",
+        options   = {
+          awslogs-group         = "ubuntu-container",
+          awslogs-region        = "us-east-1",
+          awslogs-create-group  = "true",
           awslogs-stream-prefix = "ecs"
         }
       }
       #      portMappings = [
-#        {
-#          containerPort = 80
-#          hostPort      = 80
-#        }
-#      ]
+      #        {
+      #          containerPort = 80
+      #          hostPort      = 80
+      #        }
+      #      ]
     },
   ])
 }
